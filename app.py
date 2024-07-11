d334e3b53bc41f19e4e21aaa9d5c6ebf3550033e
Darshak Vasoya <darshakvasoya1310@gmail.com>
2024-07-12 00:08:11 +0200

    Initial Commit

---
+++ app.py
@@ -0,0 +1,42 @@
+
+from flask import Flask,request, jsonify
+import json
+
+app=Flask(__name__)
+
+
+# load the manga data from json file
+with open('Data/data.json','r') as file:
+    manga_data=json.load(file)
+
+@app.route('/',methods=['GET'])
+def get_all_manhua():
+    return jsonify(manga_data)
+
+@app.route('/manga/<title>', methods=['GET'])
+def get_manhua_by_title(title):
+    for manga in manga_data:
+        if manga['title'].lower() == title.lower():
+            return jsonify(manga)
+    return jsonify({"error": "Manga not found"}), 404
+
+
+@app.route('/manga/<title>/chapters', methods=['GET'])
+def get_chapters(title):
+    for manga in manga_data:
+        if manga['title'].lower() == title.lower():
+            return jsonify(manga['chapters'])
+    return jsonify({"error": "Manga not found"}), 404
+
+@app.route('/manga/<title>/chapters/<chapter>', methods=['GET'])
+def get_chapter(title, chapter):
+    for manga in manga_data:
+        if manga['title'].lower() == title.lower():
+            if chapter in manga['chapters']:
+                return jsonify(manga['chapters'][chapter])
+            else:
+                return jsonify({"error": "Chapter not found"}), 404
+    return jsonify({"error": "Manga not found"}), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
